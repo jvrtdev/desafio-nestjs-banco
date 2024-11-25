eb764ba984b816ce4b546ade6e9f07086b86e28a
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-11-25 16:44:30 -0300

    :package: feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Desafio NestJS - João Vitor')
+    .setDescription(
+      'Simulação de funcionamento de um sistema bancário com implementação de conceitos de Domain-Driven Design (DDD). Usei padrões de arquitetura e boas práticas de desenvolvimento que aprendi ao longo da minha carreira como desenvolvedor Nest.js',
+    )
+    .setVersion('1.0')
+    .addTag('desafio-nestjs')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
