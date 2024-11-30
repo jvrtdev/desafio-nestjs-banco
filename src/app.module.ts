9cae41d0de4888a648af38c5c35a402f67ad52fd
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-11-30 18:06:31 -0300

    refactor: add modules from application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './infrastructure/database/prisma/prisma.module';
+import { DatabaseModule } from './infrastructure/database/database.module';
+import { AccountModule } from './modules/account/account.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [],
-  providers: [],
+  imports: [DatabaseModule, CustomerModule, AccountModule, TransactionModule],
 })
 export class AppModule {}
