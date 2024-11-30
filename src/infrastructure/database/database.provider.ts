c30b60edc20895257ec249fe462a4d2f3399fc39
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-11-30 17:55:13 -0300

    feat: initialize database

---
+++ src/infrastructure/database/database.provider.ts
@@ -0,0 +1,30 @@
+import { Sequelize } from 'sequelize-typescript';
+import { Account } from 'src/domain/entities/account/account.entity';
+import { Customer } from 'src/domain/entities/customer/customer.entity';
+import { TransactionAccount } from 'src/domain/entities/transaction-account/transaction-account.entity';
+import { Transaction } from 'src/domain/entities/transaction/transaction.entity';
+
+export const databaseProviders = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () => {
+      const sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'desafio-nestjs',
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000,
+        },
+      });
+      sequelize.addModels([Customer, Account, Transaction, TransactionAccount]);
+      await sequelize.sync({ force: true });
+      return sequelize;
+    },
+  },
+];
