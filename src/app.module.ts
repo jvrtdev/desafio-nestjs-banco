f13bc9ceccbe5059a31f464d447e29c1bd8560ea
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-12-01 00:11:35 -0300

    refactor: add all modules off app(transactionAccountModule)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { AccountModule } from './modules/account/account.module';
 import { CustomerModule } from './modules/customer/customer.module';
+import { TransactionAccountModule } from './modules/transaction-account/transaction-account.module';
 import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
-  imports: [DatabaseModule, CustomerModule, AccountModule, TransactionModule],
+  imports: [
+    DatabaseModule,
+    CustomerModule,
+    AccountModule,
+    TransactionModule,
+    TransactionAccountModule,
+  ],
 })
 export class AppModule {}
